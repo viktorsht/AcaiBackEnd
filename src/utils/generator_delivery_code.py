67a5bc361f239d59c3630df302d345c8430e09a3
viktorsht <viktorshantus@gmail.com>
2023-04-27 22:52:09 -0300

    Refatorando API e removendo arquivos desnecessários

--- src/utils/generator_delivery_code.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def generate_delivery_code():
-    code = random.randint(1000, 9999)
-    return code
-
-#print(f"A senha do seu pedido é: {generate_delivery_code()}")
--- src/utils/time.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-def generate_time():
-    now = datetime.datetime.now()
-    return now.strftime("%Y-%m-%d %H:%M:%S")
